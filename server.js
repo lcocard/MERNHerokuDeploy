43b5a22d8ac923b9f8d3ff3fd9c33d9098182fd7
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-10-17 21:34:46 -0400

    create heroku app and add mongo

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user10:user10@ds335648.mlab.com:35648/heroku_mv2glh10");
 
 // Start the API server
 app.listen(PORT, function() {
